3e430ce8fc8a496c9238f9459ad8e3eb35ea87f4
loganhz <logan@rancher.com>
2020-03-27 10:33:20 +0800

    Add rancher change

--- aliyunecs/ecs.go
+++ aliyunecs/ecs.go
@@ -34,7 +34,7 @@ const (
 	defaultRootSize          = 20
 	internetChargeType       = string(common.PayByTraffic)
 	ipRange                  = "0.0.0.0/0"
-	machineSecurityGroupName = "docker-machine"
+	machineSecurityGroupName = "rancher-machine"
 	vpcCidrBlock             = "10.0.0.0/8"
 	vSwitchCidrBlock         = "10.1.0.0/24"
 	timeout                  = 300
@@ -1106,7 +1106,7 @@ func (d *Driver) configureSecurityGroup(vpcId string, groupName string) error {
 		creationArgs := ecs.CreateSecurityGroupArgs{
 			RegionId:          d.Region,
 			SecurityGroupName: groupName,
-			Description:       "Docker Machine",
+			Description:       "Rancher Machine",
 			VpcId:             vpcId,
 			ClientToken:       d.getClient().GenerateClientToken(),
 		}
@@ -1230,6 +1230,54 @@ func (d *Driver) configureSecurityGroupPermissions(group *ecs.DescribeSecurityGr
 		IpRange:    ipRange,
 	})
 
+	//rke begin
+	//apiserver
+	perms = append(perms, IpPermission{
+		IpProtocol: ecs.IpProtocolTCP,
+		FromPort:   6443,
+		ToPort:     6443,
+		IpRange:    ipRange,
+	})
+
+	//etcd
+	perms = append(perms, IpPermission{
+		IpProtocol: ecs.IpProtocolTCP,
+		FromPort:   2379,
+		ToPort:     2380,
+		IpRange:    ipRange,
+	})
+
+	//kubelet ScedulerPort ControllerPort
+	perms = append(perms, IpPermission{
+		IpProtocol: ecs.IpProtocolTCP,
+		FromPort:   10250,
+		ToPort:     10252,
+		IpRange:    ipRange,
+	})
+
+	//KubeProxyPort
+	perms = append(perms, IpPermission{
+		IpProtocol: ecs.IpProtocolTCP,
+		FromPort:   10256,
+		ToPort:     10256,
+		IpRange:    ipRange,
+	})
+
+	perms = append(perms, IpPermission{
+		IpProtocol: ecs.IpProtocolUDP,
+		FromPort:   4789,
+		ToPort:     4789,
+		IpRange:    ipRange,
+	})
+
+	perms = append(perms, IpPermission{
+		IpProtocol: ecs.IpProtocolUDP,
+		FromPort:   8472,
+		ToPort:     8472,
+		IpRange:    ipRange,
+	})
+
+	//rke end
 	//如果是容器网段的话，需要设置容器网段开放安全组
 	if d.VpcId != "" || d.VSwitchId != "" {
 		containerIPRange, err := getContainerCIDR(d.RouteCIDR)
